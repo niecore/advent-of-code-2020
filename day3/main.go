f1dcc46bb7556a091cd1060ed7a8fb3ef128f50d
niecore <nicomueller1991@googlemail.com>
2020-12-03 19:43:58 +0100

    Day 3 Part 2 + multiple bug fixes

--- day3/main.go
+++ day3/main.go
@@ -19,22 +19,37 @@ type PositionDelta struct {
 func main() {
 	linesOfInputFile := util.ReadInput("day3/input.txt")
 
-	pos := Position{0, 0}
-	delta := PositionDelta{1, 3}
-	endPos :=	calculateFinalPosition(len(linesOfInputFile), pos, delta)
-	treesHit := 0
-
-	for {
-		if positionIsTree(pos, linesOfInputFile) {
-			treesHit++
-		}
-
-		pos = calculateNewPosition(pos, delta)
-
-		if pos == endPos{break}
+	deltas := []PositionDelta{
+		PositionDelta{1, 1},
+		PositionDelta{1, 3},
+		PositionDelta{1, 5},
+		PositionDelta{1, 7},
+		PositionDelta{2, 1},
 	}
 
-	fmt.Printf("During your way you hit in total %d trees \n", treesHit)
+	puzzleInput := 1
+
+	for i, delta := range deltas {
+		pos := Position{0, 0}
+		endPos :=	calculateFinalPosition(len(linesOfInputFile) -1, pos, delta)
+		treesHit := 0
+
+		for {
+			if positionIsTree(pos, linesOfInputFile) {
+				treesHit++
+			}
+
+			if pos == endPos{
+				break
+			} else {
+				pos = calculateNewPosition(pos, delta)
+			}
+		}
+		fmt.Printf("During your way in iteration %d you hit in total %d trees \n", i, treesHit)
+		puzzleInput = puzzleInput * treesHit
+	}
+
+	fmt.Printf("Your Puzzle Input is %d\n", puzzleInput)
 	os.Exit(0)
 }
 
@@ -43,12 +58,12 @@ func calculateNewPosition(pos Position, delta PositionDelta) Position {
 }
 
 func positionIsTree(pos Position, areaMap []string) bool {
-		return areaMap[pos.X][pos.Y % len(areaMap[0])] == "#"[0]
+	return areaMap[pos.X][pos.Y % len(areaMap[0])] == "#"[0]
 }
 
 func calculateFinalPosition(length int, pos Position, delta PositionDelta) Position {
 	verticalSteps := length / (delta.X - pos.X)
 	horizontalSteps := verticalSteps * delta.Y
 
-	return Position{verticalSteps, horizontalSteps}
+	return Position{length, horizontalSteps}
 }
